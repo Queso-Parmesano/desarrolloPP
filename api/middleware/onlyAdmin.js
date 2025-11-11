8579d37e6ed3078e116abc4d386ce7c21d7e5149
QuesoParmesano <pecazon03@gmail.com>
2025-11-11 00:12:32 -0300

    Actualizacion

--- api/middleware/onlyAdmin.js
+++
@@ -1,9 +0,0 @@
-
-function onlyAdmin(req, res, next) {
-    if(req.user.rol !== 'admin'){
-        return res.status(403).json({message: 'Access denied'})
-    }
-    next()
-}
-
-export default onlyAdmin
